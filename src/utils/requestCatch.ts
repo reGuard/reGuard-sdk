4378fa101a2d22b3479f732e1001c76e949c6064
LDJ-go <2623555397@qq.com>
2022-08-12 14:24:34 +0800

    feat: 新增JSError和资源异常捕获

--- src/utils/requestCatch.ts
+++ src/utils/requestCatch.ts
@@ -1,11 +1,5 @@
-
-    //接口异常采集
- export const  captureRequest = ()=>{
-     let origin = XMLHttpRequest
-     XMLHttpRequest.prototype.open = function(
-        method: string, url: string | URL, async?: boolean, username?: string | null, password?: string | null
-     ){
-        
-     }
-    }
-    
+//接口异常采集
+export const captureRequest = () => {
+    let origin = XMLHttpRequest;
+    XMLHttpRequest.prototype.open = function (method: string, url: string | URL, async?: boolean, username?: string | null, password?: string | null) {};
+};
