dae8c81497a9cdc7adce454421a4dacabe0d866d
LDJ-go <2623555397@qq.com>
2022-08-12 14:53:47 +0800

    feat: 新增XHR异常捕获

--- src/utils/requestCatch.ts
+++
@@ -1,5 +0,0 @@
-//接口异常采集
-export const captureRequest = () => {
-    let origin = XMLHttpRequest;
-    XMLHttpRequest.prototype.open = function (method: string, url: string | URL, async?: boolean, username?: string | null, password?: string | null) {};
-};
